3fe3bb1029aee8ec501021c2c1b40dcc8ba4a219
Yi-Shan Wu <yswu@di.ku.dk>
2022-05-31 15:18:48 +0200

    clean up plot

--- plots/plot.py
+++ plots/plot.py
@@ -7,9 +7,6 @@ import numpy as np
 import pandas as pd
 import os
 
-BASE = sys.argv[1] if len(sys.argv)>=2 else 'rfc'
-M = int(sys.argv[2]) if len(sys.argv)>=3 else 100
-
 EXP_PATH = "../out/"
 DATASETS = {
             'haberman':'Haberman',
@@ -25,94 +22,29 @@ DATASETS = {
 
 PREC = 3
 
-PRETTY_MAP = {
-    "best_mv_risk":"$L(h_{best})$",
-    "unf_mv_risk":"$L(\\MV_{u})$",
-    "lam_mv_risk":"$L(\\MV_{\\rho_\\lambda})$",
-    "tnd_mv_risk":"$L(\\MV_{\\rho_{\\TND}})$",
-    "cctnd_mv_risk":"$L(\\MV_{\\rho_{\\CCTND}})$",
-    "ccpbb_mv_risk":"$L(\\MV_{\\rho_{\\CCPBB}})$",
-    "lam_PBkl":"$\\FO(\\rho_\\lambda)$",
-    "tnd_TND":"$\\TND(\\rho_{\\TND})$",
-    "cctnd_CCTND":"$\\CCTND(\\rho_{\\CCTND})$",
-    "ccpbb_CCPBB":"$\\CCPBB(\\rho_{\\CCPBB})$",
-    "lam":"$\\FO$",
-    "tnd":"$\\TND$",
-    "cctnd":"$\\CCTND$",
-    "ccpbb":"$\\CCPBB$",
-    "gibbs":"$\\E_\\rho[L]$",
-    "tandem":"$\\E_{\\rho^2}[L]$",
-    "bmu":"$\mu$",
-}
-
-"""
-def comparison_table(tp='risk'):
-    path = "table/"+base+"/optimize/"
-    out_fname = path+tp+"_table.tex"
+""" Plot error and bounds for several data sets """
+def multi_bounds():
+    path = "figure/datasets/"
     if not os.path.isdir(path):
         os.makedirs(path)
 
-    opts = {
-        ("risk","rfc"):       ["unf","lam","tnd","cctnd","ccpbb"],
-        ("risk","mce"):       ["unf","best","lam","tnd","cctnd","ccpbb"],
-        ("bound","rfc"):      [("lam","PBkl"),("tnd","TND"),("cctnd","CCTND"),("ccpbb","CCPBB")],
-        ("bound","mce"):      [("lam","PBkl"),("tnd","TND"),("cctnd","CCTND"),("ccpbb","CCPBB")],
-    }[(tp,base)]
-    if tp=='risk':
-        opts = [(o,"mv_risk") for o in opts]
-    
-    copts = [pre+"_"+suf for pre,suf in opts]
-    
-    if hl1=="all":
-        hl1 = copts
+    for ds in DATASETS.keys():
+        df = pd.read_csv(EXP_PATH+ds+"-"+"Avg-Ex-IP.csv",sep=",")
+        df_mean, df_std = df.mean(), df.std()
+        bounds = ["PBkl", "PBUB", "PBSkl"]
+        with open(path+ds+".tex", "w") as f:
+            for i, bnd in enumerate(bounds):
+                f.write("\\addplot["+bnd+", Bound]coordinates {("+str(i+1)+","+str(df_mean["bound_"+bnd])+") +- (0,"+str(df_std["bound_"+bnd])+")};\n")
+            for i, bnd in enumerate(bounds):
+                f.write("\\addplot["+bnd+", MyRisk]coordinates {("+str(i+1)+","+str(df_mean["Lntest_"+bnd])+") +- (0,"+str(df_std["Lntest_"+bnd])+")};\n")
 
-    with open(out_fname, 'w') as fout:
-        # Header
-        fout.write("\\begin{tabular}{l"+"c"*len(opts)+"}\\toprule\n")
-        fout.write("Data set")
-        for i,col in enumerate(copts):
-            fout.write(" & "+PRETTY_MAP[col])
-        fout.write(" \\\\\n")
-        fout.write("\\midrule\n")
-
-        for ds in DATASETS:
-            if (base == 'rfc' and ds == 'Protein'):
-                continue
-            df = pd.read_csv(EXP_PATH+ds+"-"+str(M)+"-bootstrap-iRProp.csv",sep=";")
-            df_mean = df.mean()
-            df_std  = df.std()
-            
-            # Highlight indices
-            v1 = np.min(df_mean[hl1]) if len(hl1)>0 else -1
-            v2 = np.min(df_mean[hl2]) if len(hl2)>0 else -1
-            v1 = str(round(v1,PREC))
-            v2 = str(round(v2,PREC))
-
-            fout.write("\\dataset{"+RENAME.get(ds,ds)+"}")
-            for i,col in enumerate(copts):
-                fval = df_mean[col]
-                val = str(round(fval,PREC))
-                std = str(round(df_std[col],PREC))
-                s = val + " ("+std+")"
-                if col in hl1 and val==v1:
-                    s = "\\textbf{"+s+"}"
-                if col in hl2 and val==v2:
-                    s = "\\underline{"+s+"}"
-                fout.write(" & "+s)
-            fout.write(" \\\\\n")
-
-        fout.write("\\bottomrule\n") 
-        fout.write("\\end{tabular}\n")
-"""
-#optimized_comparison_table('risk', base=BASE, hl2=["lam_mv_risk","tnd_mv_risk","cctnd_mv_risk","ccpbb_mv_risk"])
-#optimized_comparison_table('bound', base=BASE, hl2=["tnd_TND","cctnd_CCTND","ccpbb_CCPBB"])
-
+multi_bounds()
 
 """
 Making tables comparing Avg bounds:
-"""
+
 def All_table(rep_opt="bound"):
-    """ rep_opt: "bound", "Lntest", "bestSigma2" """
+    # rep_opt: "bound", "Lntest", "bestSigma2"
     path = "table/"
     out_fname = path+"All_"+rep_opt+"_table.tex"
     if not os.path.isdir(path):
@@ -163,67 +95,15 @@ def All_table(rep_opt="bound"):
             fout.write(" \\\\\n")
         fout.write("\\hline\n")
         fout.write("\\end{tabular}\n")
-        
+"""
 #All_table("bound")
 #All_table("Lntest")
 #All_table("bestSigma2")
 
-# Plot error and bounds for several data sets
-def multi_bounds():
-    path = "figure/datasets/"
-    if not os.path.isdir(path):
-        os.makedirs(path)
-
-    for ds in DATASETS.keys():
-        #df = pd.read_csv(EXP_PATH+ds+"-"+".csv",sep=",")
-        #dfFW = pd.read_csv(EXP_PATH+ds+"-"+"FW.csv",sep=",")
-        #dfAvg = pd.read_csv(EXP_PATH+ds+"-"+"Avg.csv",sep=",")
-        #df_mean, df_std = df.mean(), df.std()
-        #dfFW_mean, dfFW_std = dfFW.mean(), dfFW.std()
-        #dfAvg_mean, dfAvg_std = dfAvg.mean(), dfAvg.std()
-        dfB = pd.read_csv(EXP_PATH+ds+"-"+"Avg-Ex-IP.csv",sep=",")
-        dfB_mean, dfB_std = dfB.mean(), dfB.std()
-
-        bounds = [("PBkl","Best"), ("MGG","Best"), ("Skl","Best")]
-        with open(path+ds+".tex", "w") as f:
-            for i, (bnd,cls) in enumerate(bounds):
-                if cls=="Avg":
-                    f.write("\\addplot["+bnd+", Bound]coordinates {("+str(i+1)+","+str(dfAvg_mean["bound_"+bnd])+") +- (0,"+str(dfAvg_std["bound_"+bnd])+")};\n")
-                elif cls=="FW":
-                    f.write("\\addplot["+bnd+", Bound]coordinates {("+str(i+1)+","+str(dfFW_mean["bound_"+bnd])+") +- (0,"+str(dfFW_std["bound_"+bnd])+")};\n")
-                elif cls=="":
-                    f.write("\\addplot["+bnd+", Bound]coordinates {("+str(i+1)+","+str(df_mean["bound_"+bnd])+") +- (0,"+str(df_std["bound_"+bnd])+")};\n")
-                elif cls=="Best":
-                    f.write("\\addplot["+bnd+", Bound]coordinates {("+str(i+1)+","+str(dfB_mean["bound_"+bnd])+") +- (0,"+str(dfB_std["bound_"+bnd])+")};\n")
-                else:
-                    return 0
-            for i, (bnd,cls) in enumerate(bounds):
-                if cls=="Avg":
-                    f.write("\\addplot["+bnd+", MyRisk]coordinates {("+str(i+1)+","+str(dfAvg_mean["Lntest_"+bnd])+") +- (0,"+str(dfAvg_std["Lntest_"+bnd])+")};\n")
-                elif cls=="FW":
-                    f.write("\\addplot["+bnd+", MyRisk]coordinates {("+str(i+1)+","+str(dfFW_mean["Lntest_"+bnd])+") +- (0,"+str(dfFW_std["Lntest_"+bnd])+")};\n")
-                elif cls=="":
-                    f.write("\\addplot["+bnd+", MyRisk]coordinates {("+str(i+1)+","+str(df_mean["Lntest_"+bnd])+") +- (0,"+str(df_std["Lntest_"+bnd])+")};\n")
-                elif cls=="Best":
-                    f.write("\\addplot["+bnd+", MyRisk]coordinates {("+str(i+1)+","+str(dfB_mean["Lntest_"+bnd])+") +- (0,"+str(dfB_std["Lntest_"+bnd])+")};\n")
-                else:
-                    return 0
-
-
-multi_bounds()
-
-"""
-Investigate PBUB under IP and NOIP
-"""
-def PBUB_IP_NOIP_table():
-    path = "table/"
-    out_fname = path+"PBUB_IP_NOIP_table.tex"
-    if not os.path.isdir(path):
-        os.makedirs(path)
 
 """
 Making tables comparing Vanilla and FW of PBkl and MGG bounds:
-"""
+
 def Vanilla_FW_table():
     path = "table/"
     out_fname = path+"Vanilla_FW_table.tex"
@@ -265,11 +145,12 @@ def Vanilla_FW_table():
                 fout.write(" \\\\\n")
             fout.write("\\hline\n")
         fout.write("\\end{tabular}\n")
+"""
 #Vanilla_FW_table()
 
 """
 Making tables comparing FW and FWEL bounds:
-"""
+
 def FW_FWEL_table():
     path = "table/"
     out_fname = path+"FW_FWEL_table.tex"
@@ -306,13 +187,13 @@ def FW_FWEL_table():
                 fout.write(" \\\\\n")
             fout.write("\\hline\n")
         fout.write("\\end{tabular}\n")
-
+"""
 #FW_FWEL_table()
 
 
 """
 Making tables about values of FWEL:
-"""
+
 def FWEL_values():
     path = "table/"
     out_fname = path+"FWEL_values.tex"
@@ -351,43 +232,6 @@ def FWEL_values():
                 fout.write(" \\\\\n")
             fout.write("\\hline\n")
         fout.write("\\end{tabular}\n")
-
+"""
 #FWEL_values()
 
-"""
-def TND_Ben_comparison_table(base='rfc'):
-    path = "table/"+base+"/optimize/"
-    if not os.path.isdir(path):
-        os.makedirs(path)
-    
-    prec = 5
-    opts = ["tnd", "mu", "bern"]
-    cols = ["dataset", "c", "d"]
-    for opt in opts:
-        if opt == "tnd":
-            cols += [opt+suf for suf in ["_KL", "_gibbs", "_tandem", "_tnd", "_TandemUB"]]
-        elif opt == "mu":
-            cols += [opt+suf for suf in ["_KL", "_gibbs", "_tandem", "_MU", "_muTandemUB", "_bmu"]]
-        elif opt == "bern":
-            cols += [opt+suf for suf in ["_KL", "_gibbs", "_tandem", "_bern", '_mutandem_risk', '_vartandem_risk', "_varUB", "_bernTandemUB", "_bmu", "_bg", "_bl"]]
-    rows = []
-    for ds in DATASETS:
-        if (base == 'rfc' and ds == 'Protein'):
-            continue
-        df = pd.read_csv(EXP_PATH+ds+"-"+str(M)+"-bootstrap-iRProp.csv",sep=";")
-        df_mean = df.mean()
-        df_std  = df.std()
-        
-        row = [ds, df_mean["c"], df_mean["d"]]
-        for opt in opts:
-            if opt == "tnd":
-                row += [df_mean[opt+suf] for suf in ["_KL", "_gibbs", "_tandem", "_tnd", "_TandemUB"]]
-            elif opt == "mu":
-                row += [df_mean[opt+suf] for suf in ["_KL", "_gibbs", "_tandem", "_MU", "_muTandemUB", "_bmu"]]
-            elif opt == "bern":
-                row += [df_mean[opt+suf] for suf in ["_KL", "_gibbs", "_tandem", "_bern", '_mutandem_risk', '_vartandem_risk', "_varUB", "_bernTandemUB", "_bmu", "_bg", "_bl"]]            
-        rows.append(row)
-    
-    pd.DataFrame(data=rows, columns=cols).round(prec).to_csv(path+"mu_comparison.csv", sep=",", index=False)
-"""
-#TND_Ben_comparison_table(base=BASE)
